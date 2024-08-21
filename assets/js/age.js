a4bdf14770deb3a08cb29c055f40959306382c61
ababdelo <edunwant42@gmail.com>
2024-08-21 05:26:03 +0100

    Updating

--- assets/js/age.js
+++ assets/js/age.js
@@ -1,1 +1,2 @@
 function calculateAge(e){let t=new Date,a=new Date(e);if(a>t)return"Invalid";if(1990>a.getFullYear()||a.getFullYear()>2010)return"Birthdate out of range";let n=t.getFullYear()-a.getFullYear(),l=t.getMonth()-a.getMonth();return(l<0||0===l&&t.getDate()<a.getDate())&&n--,n}document.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("birthday"),t=e.getAttribute("datetime"),a=calculateAge(t);document.getElementById("age").textContent=a});
+console.log("age.js loaded");
---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+import './script.js';
+import './canvas.js';
+import './scrt.js';
+import './age.js';
+import './scramble.js';
+
+console.log('main.js loaded');
